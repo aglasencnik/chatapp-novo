4ca4d3fe90ea074f39de698ed849058483301ada
DjAmadej <39464103+DjAmadej@users.noreply.github.com>
2022-02-20 18:33:36 +0100

    Add files via upload

---
+++ javascript/chat.js
@@ -0,0 +1,58 @@
+const form = document.querySelector(".typing-area"),
+    inputField = form.querySelector(".input-field"),
+    sendBtn = form.querySelector("button"),
+    chatBox = document.querySelector(".chat-box");
+
+form.onsubmit = (e)=>{
+    e.preventDefault(); //preventing from submitting
+}
+
+sendBtn.onclick = ()=> {
+    //Let's start AJAX
+    let xhr = new XMLHttpRequest(); //creating XML object
+    xhr.open("POST", "php/insert-chat.php", true);
+    xhr.onload = ()=>{
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                inputField.value = ""; //once message inserted into database then leave blank in the input field
+                scrollToBottom();
+            }
+        }
+    }
+    //we have to send the form data through ajax to php
+    let formData = new FormData(form); //Creating new formData Object
+    xhr.send(formData); //sending the form data to php
+}
+
+chatBox.onmouseenter = ()=>{
+    chatBox.classList.add("active");
+}
+
+chatBox.onmouseleave = ()=>{
+    chatBox.classList.remove("active");
+}
+
+setInterval(() => {
+    //Let's start AJAX
+    let xhr = new XMLHttpRequest(); //creating XML object
+    xhr.open("POST", "php/get-chat.php", true);
+    xhr.onload = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                let data = xhr.response;
+                chatBox.innerHTML = data;
+                if (!chatBox.classList.contains("active")){ //if active class not contains in chatbox the scroll to bottom
+                    scrollToBottom();
+                }
+            }
+        }
+    }
+
+    //we have to send the form data through ajax to php
+    let formData = new FormData(form); //Creating new formData Object
+    xhr.send(formData); //sending the form data to php
+}, 500); //this function will run frequently after 500ms
+
+function scrollToBottom(){
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
